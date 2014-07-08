b1b0d6fc469ec6b2ac9ce66dd45bc903f6603bc9
Justin Sousa <jsousa1919@gmail.com>
2014-07-08 01:54:46 +0000

    Bootstrap script

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get install `cat requirements`
+pip install -r dev-requirements.pip
+sudo su postgres <<EOF
+psql -c "create database paypertrade";
+psql -c "create user paypertrade with password 'paypertrade'";
+psql -c "grant all on database paypertrade to paypertrade";   
+EOF
+cp servedev.conf.default servedev.conf
+scripts/buildconf.sh
+alembic migrate HEAD
+scripts/servedev.sh

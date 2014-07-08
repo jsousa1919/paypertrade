fffc1cbf6efd4c620458b092c1763ede65be11e2
Justin Sousa <jsousa1919@gmail.com>
2014-07-08 04:03:26 +0000

    Ah whatever

--- paypertrade/controllers/base.py
+++ paypertrade/controllers/base.py
@@ -13,11 +13,6 @@ log = logging.getLogger(__name__)
 class BaseController(BaseController):
     def index(self):
         c.users = model.Session.query(model.User).all()
-        fake1 = object()
-        fake1.name = 'I do have objects'
-        fake1.email = 'just a list of objects'
-        fake1.username = 'all the same'
-        c.fakes = [fake1]
         return render_mako('index.mako')
 
     def template_error(self):

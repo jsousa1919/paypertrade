4e521c3086da926f0e117be89dcbe4d365504ba5
Justin Sousa <jsousa1919@gmail.com>
2014-07-08 03:29:46 +0000

    maybeeee

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,7 @@ psql -c "grant all on database paypertrade to paypertrade";
 EOF
 cp servedev.conf.default servedev.conf
 scripts/buildconf.sh
-echo "export PYTHONPATH=`pwd`" >> ~/.bashrc
+echo "export PYTHONPATH=$PYTHONPATH:`pwd`" >> ~/.bashrc
 source ~/.bashrc
 alembic upgrade head
 scripts/servedev.sh
